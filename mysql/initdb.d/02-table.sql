13a4f50f860fb0db60c75cb9ea263bdde2f6a0c1
NishiokaKazuki <n.kazuki.hal@gmail.com>
2020-03-17 15:02:40 +0900

    初期設定

---
+++ mysql/initdb.d/02-table.sql
@@ -0,0 +1,43 @@
+SET CHARSET utf8;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users
+(
+    id               bigint unsigned AUTO_INCREMENT,
+    name             text NOT NULL,
+    disabled         boolean DEFAULT false,
+    created_at       timestamp NOT NULL DEFAULT current_timestamp,
+    updated_at       timestamp NOT NULL DEFAULT current_timestamp on update current_timestamp,
+    PRIMARY KEY (id)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS work_times;
+
+CREATE TABLE work_times
+(
+    id               bigint unsigned AUTO_INCREMENT,
+    user_id          bigint unsigned NOT NULL,
+    content          text unique NOT NULL,
+    supplement       text NOT NULL,
+    disabled         boolean DEFAULT false,
+    started_at       timestamp NOT NULL DEFAULT current_timestamp,
+    finished_at      timestamp NOT NULL DEFAULT current_timestamp on update current_timestamp,
+    FOREIGN KEY (user_id)
+    REFERENCES users(id),
+    PRIMARY KEY (id)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS work_rests;
+
+CREATE TABLE work_times
+(
+    id               bigint unsigned AUTO_INCREMENT,
+    work_time_id     bigint unsigned NOT NULL,
+    disabled         boolean DEFAULT false,
+    started_at       timestamp NOT NULL DEFAULT current_timestamp,
+    finished_at      timestamp NOT NULL DEFAULT current_timestamp on update current_timestamp,
+    FOREIGN KEY (work_time_id)
+    REFERENCES work_times(id),
+    PRIMARY KEY (id)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
---
+++ server/go.mod
@@ -0,0 +1,3 @@
+module example.com/WorkTimeRecoder
+
+go 1.13
