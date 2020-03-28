a7dff8dd0517e1594b63ab12a939735bed8a89cd
NishiokaKazuki <n.kazuki.hal@gmail.com>
2020-03-29 03:30:55 +0900

    refs#0000 helpメッセージの追記

--- server/api/server.go
+++ server/api/server.go
@@ -255,7 +255,15 @@ func PrefixMessage(message string) (string, error) {
 	log.Println(message)
 	switch message {
 	default:
-		res = "Command List\n"
+		res = "やあ僕きもかわいいgopher君!僕は以下の機能を持ってるよ活用してね!\n\n" +
+			"作業記録機能(例)\n" +
+			"1.開始 `hogehoge` -tm\n" +
+			"2.中断 `hogehoge` -t\n" +
+			"3.再開 `hogehoge` -t\n" +
+			"4.終了 `hogehoge` -t\n\n" +
+			"option list\n" +
+			"-t  : 時間を指定可能 `yyyy/mm/dd hh:mm`, `hh:mm` のフォーマット\n" +
+			"-m  : コメントを追記可能 ただし半角空白で区切られる\n"
 	}
 
 	return res, nil
