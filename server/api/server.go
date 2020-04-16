01ffaaabfdd88c0df29dd5c2ea62be9cde5796ef
NishiokaKazuki <n.kazuki.hal@gmail.com>
2020-04-16 22:26:37 +0900

    refs#1024 helpメッセージ等を別で管理

--- server/api/server.go
+++ server/api/server.go
@@ -6,6 +6,7 @@ import (
 	"server/auth"
 	"server/config"
 	"server/model/db"
+	"server/model/sentence"
 	"server/model/table"
 	"server/query"
 	"server/utils"
@@ -290,16 +291,12 @@ func PrefixMessage(message string) (string, error) {
 	var res string
 
 	switch message {
+	case "fuck":
+		res = sentence.Fuck
+	case "ブディの真似して":
+		res = sentence.Budi
 	default:
-		res = "やあ僕きもかわいいgopher君!僕は以下の機能を持ってるよ活用してね!\n\n" +
-			"作業記録機能(例)\n" +
-			"1.開始 `hogehoge` -tm\n" +
-			"2.中断 `hogehoge` -t\n" +
-			"3.再開 `hogehoge` -t\n" +
-			"4.終了 `hogehoge` -t\n\n" +
-			"option list\n" +
-			"-t  : 時間を指定可能 `yyyy-mm-dd hh:mm`, `hh:mm` のフォーマット\n" +
-			"-m  : コメントを追記可能 ただし半角空白で区切られる\n"
+		res = sentence.Greeting
 	}
 
 	return res, nil
---
+++ server/model/sentence/sentence.go
@@ -0,0 +1,16 @@
+package sentence
+
+const (
+	Greeting = "やあ"
+	Fuck     = "Fuck"
+	Budi     = "うえええええええええええええええええええええ"
+	Help     = "やあ僕きもかわいいgopher君!僕は以下の機能を持ってるよ活用してね!\n\n" +
+		"作業記録機能(例)\n" +
+		"1.開始 `hogehoge` -tm\n" +
+		"2.中断 `hogehoge` -t\n" +
+		"3.再開 `hogehoge` -t\n" +
+		"4.終了 `hogehoge` -t\n\n" +
+		"option list\n" +
+		"-t  : 時間を指定可能 `yyyy-mm-dd hh:mm`, `hh:mm` のフォーマット\n" +
+		"-m  : コメントを追記可能 ただし半角空白で区切られる\n"
+)
